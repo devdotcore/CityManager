74ccf3a4798d08a393ea30ce64f280f4b01a9c3b
Kuldeep S Bhakuni <kuldeep.bhakuni@blackcatsolutions>
2020-07-08 07:25:16 +0100

    Added Swagger Implementatio

--- src/CityManager/Startup.cs
+++ src/CityManager/Startup.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using CityManager.Helper;
+using CityManager.Model;
+using CityManager.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CityManager
 {
@@ -25,7 +31,33 @@ namespace CityManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "City Manager",
+                    Description = "Perform CRUD operations on a city"
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.Configure<AppSettings>(Configuration);
+
+            // Add HTTP Client
+            // services.AddHttpClient(nameof(CountriesApi), c =>
+            // {
+            //     c.BaseAddress = new Uri(Configuration.GetValue<string>($"{nameof(CountriesApi)}:{nameof(CountriesApi.Url)}"));
+            // }).AddTypedClient(c => Refit.RestService.For<IRestApiClient<Country, CountryFieldsFilter, string>>(c));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +67,14 @@ namespace CityManager
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Manager V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
